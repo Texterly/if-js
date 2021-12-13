9b433f049275ff6a226caaa9f0bd6dc2cf38aad3
Texetrly <texterly@yandex.ru>
2021-12-13 18:19:44 +0300

    added new task in lesson-4

--- main.js
+++ main.js
@@ -200,3 +200,22 @@ console.log(sum(5)(2)); // 7
 // по первому нажатию на абзац он должен покраситься в первый цвет из массива, по второму нажатию - во второй и так далее;
 // цвета из массива меняются бесконечно;
 // все абзацы работают независимо.
+
+/*
+rotate array
+Rotate an array of n elements to the right by k steps.
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7]
+is rotated to [5,6,7,1,2,3,4]. How many different ways do you know
+to solve this problem?
+ */
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+console.log(array);
+
+const rotateArray = (randomArray, k) => {
+    for (let i = 0; i < k; i++) {
+        randomArray.unshift(randomArray.pop());
+    }
+    return randomArray;
+}
+console.log(rotateArray(array,3));
